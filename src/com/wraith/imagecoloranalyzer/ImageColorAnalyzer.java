b75b9bfa1da5ba9adc8ea0c3118e7c517da2ba2c
Dmytro Dzhuha <dimadjuga@gmail.com>
2022-07-16 18:18:12 +0200

    v0.0.3
    - getAverageColor
    - getDominantColor
    - Inner classes
    - Documentation
    - Optimization

--- src/com/wraith/imagecoloranalyzer/ImageColorAnalyzer.java
+++ src/com/wraith/imagecoloranalyzer/ImageColorAnalyzer.java
@@ -12,70 +12,228 @@ import java.util.List;
  */
 public class ImageColorAnalyzer
 {
+    /**
+     * Used for algorithm selection
+     */
     enum ANALYZER_ALGORITHM
     {
         ALG_MEDIAN_CUT,
         ALG_K_MEANS
     }
+
+    /**
+     * Color data for K-Mean algorithm
+     */
+    private class CentroidData
+    {
+        /**
+         * R color - X position
+         */
+        private final int R;
+        /**
+         * G color - Y position
+         */
+        private final int G;
+        /**
+         * B color - Z position
+         */
+        private final int B;
+
+        /**
+         * Color centroid
+         */
+        private Centroid centroid;
+
+        /**
+         * Constructor
+         * @param color Color array [R, G, B]
+         */
+        public CentroidData(int[] color)
+        {
+            R = color[0];
+            G = color[1];
+            B = color[2];
+
+            centroid = null;
+        }
+
+        /**
+         * Getter
+         * @return X position (R color)
+         */
+        public int getR() { return R; }
+
+        /**
+         * Getter
+         * @return Y position (G color)
+         */
+        public int getG() { return G; }
+
+        /**
+         * Getter
+         * @return Z position (B color)
+         */
+        public int getB() { return B; }
+
+        /**
+         * Getter
+         * @return Color Centroid
+         */
+        public Centroid getCentroid() { return centroid; }
+
+        /**
+         * Set color centroid
+         * @param centroid Centroid
+         */
+        public void setCentroid(Centroid centroid) { this.centroid = centroid; }
+    }
+
+    /**
+     * Centroid class
+     */
+    private class Centroid
+    {
+        /**
+         * X position of centroid
+         */
+        private int X;
+
+        /**
+         * Y position of centroid
+         */
+        private int Y;
+
+        /**
+         * Z position of centroid
+         */
+        private int Z;
+
+
+        /**
+         * Get distance between centroid and color
+         * @param centroidData Color to find distance
+         * @return Distance between points
+         */
+        public double getDistance(CentroidData centroidData)
+        {
+            return Math.sqrt(
+                    (Math.pow(X - centroidData.getR(), 2) +
+                            Math.pow(Y - centroidData.getG(), 2) +
+                            Math.pow(Z - centroidData.getB(), 2)));
+
+        }
+
+        /**
+         * Setter
+         * @param X X position
+         */
+        public void setX(int X) { this.X = X; }
+
+        /**
+         * @param Y Y position
+         */
+        public void setY(int Y) { this.Y = Y; }
+
+        /**
+         * @param Z Z Position
+         */
+        public void setZ(int Z) { this.Z = Z; }
+    }
+
     /**
      * Image
      */
-    private BufferedImage image;
+    private BufferedImage IMG;
     /**
      * Quality of color selection
      */
     private int ALG_QUALITY;
     /**
+     * Image average color
+     */
+    private int[] IMG_AVERAGE_COLOR;
+    /**
+     * Image dominant color
+     */
+    private int[] IMG_DOMINANT_COLOR;
+    /**
      * Image pixels RGB data
      */
-    private List<int[]> IMG_DATA;
+    private final ArrayList<int[]> IMG_DATA;
     /**
      * Image color palettes
      */
-    private List<List<int[]>> IMG_PALETTES;
+    private final ArrayList<List<int[]>> IMG_PALETTES;
+
+    /**
+     * Constructor
+     * @throws IOException Image does not found
+     */
+    public ImageColorAnalyzer() throws IOException { this(null, 10); }
+
+    /**
+     * Constructor
+     * @param ALG_QUALITY Quality of color selection
+     * @throws IOException Image does not found
+     */
+    public ImageColorAnalyzer(int ALG_QUALITY) throws IOException { this(null, ALG_QUALITY); }
 
     /**
      * Constructor
      * @param path Path to file
+     * @throws IOException Image does not found
      */
-    public ImageColorAnalyzer(String path) { this(path, 10); }
+    public ImageColorAnalyzer(String path) throws IOException { this(path, 10); }
 
     /**
      * Constructor
      * @param path Path to file
      * @param ALG_QUALITY Quality of color selection
+     * @throws IOException Image does not found
      */
-    public ImageColorAnalyzer(String path, int ALG_QUALITY)
+    public ImageColorAnalyzer(String path, int ALG_QUALITY) throws IOException
     {
-        try
-        {
-            this.ALG_QUALITY = ALG_QUALITY;
-            IMG_DATA = new ArrayList<>();
-            IMG_PALETTES = new ArrayList<>();
+        IMG_DATA = new ArrayList<>();
+        this.ALG_QUALITY = ALG_QUALITY;
+        IMG_PALETTES = new ArrayList<>();
 
-            image = ImageIO.read(new File(path));
-        }
-        catch (IOException e) { System.out.println(e.getMessage()); }
+        if(path != null)
+            openImage(path);
+    }
+
+    /**
+     * Used to change image
+     * @param path Path to image
+     * @throws IOException Thrown when image does not find
+     */
+    public void changeImage(String path) throws IOException
+    {
+        IMG = null;
+        openImage(path);
+        resetImageData();
     }
 
     /**
      * Get image pixels RGB data
      */
-    public void getImageData()
+    public void getImageData() throws Exception
     {
+        if(IMG == null)
+            throw new Exception("Image doesn't loaded.");
+
         if(IMG_DATA.size() != 0)
             return;
 
         int bitColor, r, g, b;
 
-        int IMG_WIDTH = image.getWidth();
-        int IMG_HEIGHT = image.getHeight();
+        int IMG_WIDTH = IMG.getWidth();
+        int IMG_HEIGHT = IMG.getHeight();
 
         for(int x = 0; x < IMG_WIDTH; x += ALG_QUALITY)
         {
             for(int y = 0; y < IMG_HEIGHT; y += ALG_QUALITY)
             {
-                bitColor = image.getRGB(x, y);
+                bitColor = IMG.getRGB(x, y);
 
                 r = getRedChannel(bitColor);
                 g = getGreenChannel(bitColor);
@@ -89,48 +247,84 @@ public class ImageColorAnalyzer
 
     /**
      * Get color palettes from BufferedImage
-     * @param colors Amout of colors to get
+     * @param colors Amount of colors to get
      * @param algorithm Algorithm to use
      * @return ArrayList of palettes
      */
-    public ArrayList<int[]> getPalette(int colors, ANALYZER_ALGORITHM algorithm)
+    public ArrayList<int[]> getPalette(int colors, ANALYZER_ALGORITHM algorithm) throws Exception
     {
-        ArrayList<int[]> result = new ArrayList<>();
-
         if(IMG_DATA.isEmpty())
-            return result;
+            throw new Exception("First call getImageData()");
+
+        ArrayList<int[]> result = new ArrayList<>();
 
         if(algorithm == ANALYZER_ALGORITHM.ALG_MEDIAN_CUT)
             medianCut(IMG_DATA, getTwoPow(colors));
+
         else if(algorithm == ANALYZER_ALGORITHM.ALG_K_MEANS)
             kMeans(IMG_DATA, colors);
 
+        int dominantColorIdx = computeDominantColor();
+
         for (List<int[]> palette: IMG_PALETTES)
-        {
-            if(colors-- == 0)
-                return result;
+            result.add(computeAverageColor(palette));
 
-            int r = 0, g = 0, b = 0;
-            int size = palette.size();
-
-            for(int[] color : palette)
-            {
-                r += color[0];
-                g += color[1];
-                b += color[2];
-            }
-
-            r /= size;
-            g /= size;
-            b /= size;
-
-
-            result.add(new int[] {r, g, b});
-        }
-
-        result.sort((o1, o2) -> Integer.compare(o1[0], o2[0]) + Integer.compare(o1[1], o2[1]) + Integer.compare(o1[2], o2[2]));
+        setDominantColor(result.get(dominantColorIdx));
+        result.sort((firstColor, secondColor) -> Integer.compare(firstColor[0], secondColor[0]) + Integer.compare(firstColor[1], secondColor[1]) + Integer.compare(firstColor[2], secondColor[2]));
 
         return result;
+    }
+
+    /**
+     * Get image average color
+     * @return Average color from image in format [R, G, B]
+     * @throws Exception Thrown when getImageData was not called
+     */
+    public int[] getAverageColor() throws Exception
+    {
+        if(IMG_AVERAGE_COLOR != null)
+            return IMG_AVERAGE_COLOR;
+
+        if(IMG_DATA == null)
+            throw new Exception("getImageData was not called yet!");
+
+        IMG_AVERAGE_COLOR = computeAverageColor(IMG_DATA);
+
+        return IMG_AVERAGE_COLOR;
+    }
+
+    /**
+     * Get image dominant color
+     * @return Dominant color from image in format [R, G, B]
+     * @throws Exception Thrown when getPalette was not called
+     */
+    public int[] getDominantColor() throws Exception
+    {
+        if(IMG_DOMINANT_COLOR == null)
+            throw new Exception("getPalette need to be called first!");
+
+        return IMG_DOMINANT_COLOR;
+    }
+
+    /**
+     * Getter for ALG_QUALITY
+     * @return Color selection quality
+     */
+    public int getQuality() { return ALG_QUALITY; }
+
+    /**
+     * Setter for ALG_QUALITY
+     * @param ALG_QUALITY Color selection quality
+     * @return false/true
+     */
+    public boolean setQuality(int ALG_QUALITY)
+    {
+        if(ALG_QUALITY < 1 || ALG_QUALITY > 10)
+            return false;
+
+        this.ALG_QUALITY = ALG_QUALITY;
+
+        return true;
     }
 
     /**
@@ -140,6 +334,9 @@ public class ImageColorAnalyzer
      */
     private void kMeans(List<int[]> palettes, int colors)
     {
+        if(colors < 2)
+            colors = 2;
+
         HashMap<Centroid, List<CentroidData>> model = computeKMeans(palettes, colors);
         IMG_PALETTES.clear();
 
@@ -174,7 +371,7 @@ public class ImageColorAnalyzer
         {
             int randIdx = random.nextInt(imgData.size());
 
-            centroids[i] = new Centroid((i + 1));
+            centroids[i] = new Centroid();
             centroids[i].setX(imgData.get(randIdx)[0]);
             centroids[i].setY(imgData.get(randIdx)[1]);
             centroids[i].setZ(imgData.get(randIdx)[2]);
@@ -229,7 +426,6 @@ public class ImageColorAnalyzer
                 if(size == 0)
                     continue;
 
-//                System.out.println("Foreach Centroid #" + mapElement.getKey().number + " " + mapElement.getValue().size());
                 sumX = sumY = sumZ = 0;
 
                 for(CentroidData color : mapElement.getValue())
@@ -245,8 +441,6 @@ public class ImageColorAnalyzer
                 sumY /= size;
                 sumZ /= size;
 
-//                System.out.println("Centroid #" + mapCentroid.number + "\nPrev pos: " + mapCentroid.getX() + ", " + mapCentroid.getY() + ", " + mapCentroid.getZ() + "\nNew pos: " + sumX + ", " + sumY + ", " + sumZ + "\n");
-
                 mapCentroid.setX(sumX);
                 mapCentroid.setY(sumY);
                 mapCentroid.setZ(sumZ);
@@ -254,11 +448,6 @@ public class ImageColorAnalyzer
 
             exit = true;
         }
-
-        for(int i = 0; i < colors; i++)
-            System.out.println(model.get(centroids[i]).size());
-
-        System.out.println("Exit");
 
         return model;
     }
@@ -309,7 +498,7 @@ public class ImageColorAnalyzer
         else if(gRange >= rRange && gRange >= bRange)
             sortPalette(1, palettes, 0, palettes.size() - 1);
 
-        else if(bRange >= rRange && bRange >= gRange)
+        else
             sortPalette(2, palettes, 0, palettes.size() - 1);
 
         int half = (palettes.size() + 1) / 2;
@@ -357,7 +546,44 @@ public class ImageColorAnalyzer
     }
 
     /**
-     * Get power of two
+     * Get average color from palette
+     * @param palette Palette to find average color
+     * @return Average color from palette
+     */
+    private int[] computeAverageColor(List<int[]> palette)
+    {
+        int R = 0, G = 0, B = 0, size = palette.size();
+
+        if(size == 0)
+            return new int[] {-1, -1, -1};
+
+
+        for (int[] color: palette)
+        {
+            R += color[0];
+            G += color[1];
+            B += color[2];
+        }
+
+        R /= size;
+        G /= size;
+        B /= size;
+
+        return new int[] {R, G, B};
+    }
+
+    /**
+     * Used to compute dominant color palette
+     * @return Index of dominant color palette
+     */
+    private int computeDominantColor()
+    {
+        List<int[]> maxPalette = IMG_PALETTES.stream().max(Comparator.comparingInt(List::size)).orElse(null);
+        return (maxPalette == null ? -1 : IMG_PALETTES.indexOf(maxPalette));
+    }
+
+    /**
+     * Get power of two. Used for median-cut algorithm
      * @param colors Number that isn't power of 2
      * @return Power of 2 that is greater than @param colors
      */
@@ -375,38 +601,44 @@ public class ImageColorAnalyzer
     }
 
     /**
-     * Getter for ALG_QUALITY
-     * @return Color selection quality
+     * Set dominant color
+     * @param IMG_DOMINANT_COLOR RGB Array of dominant color
      */
-    public int getQuality() { return ALG_QUALITY; }
+    private void setDominantColor(int[] IMG_DOMINANT_COLOR) { this.IMG_DOMINANT_COLOR = IMG_DOMINANT_COLOR; }
 
     /**
-     * Setter for ALG_QUALITY
-     * @param ALG_QUALITY Color selection quality
-     * @return false/true
+     * Open image
+     * @param path Path to the image
+     * @throws IOException Thrown when image doesn't find
      */
-    public boolean setQuality(int ALG_QUALITY)
+    private void openImage(String path) throws IOException { IMG = ImageIO.read(new File(path)); }
+
+    /**
+     * Used to reset all variables
+     */
+    private void resetImageData()
     {
-        if(ALG_QUALITY < 1 || ALG_QUALITY > 10)
-            return false;
+        IMG_AVERAGE_COLOR =
+        IMG_DOMINANT_COLOR = null;
 
-        this.ALG_QUALITY = ALG_QUALITY;
-
-        return true;
+        IMG_DATA.clear();
+        IMG_PALETTES.clear();
     }
 
     /**
-     * Get RED chanell
+     * Get RED channel
      * @param bitColor Color in bits
      * @return Red channel
      */
     private int getRedChannel(int bitColor) { return (bitColor & 0xFF0000) >> 16; }
+
     /**
      * Get GREEN channel
      * @param bitColor Color in bits
      * @return Green channel
      */
     private int getGreenChannel(int bitColor) { return (bitColor & 0xFF00) >> 8; }
+
     /**
      * Get BLUE channel
      * @param bitColor Color in bits
